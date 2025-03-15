6200afac5645b9ece38cb802ea38e37f9632672e
Disha Gupta <disha29082001@gmail.com>
2025-03-15 10:55:04 +0530

    vercel fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-	  extend: {},
+	  extend: {
+		backgroundImage: {
+		  'grid': "url('/src/assets/grid.svg')", // Ensure this file exists
+		},
+	  },
 	},
 	plugins: [],
   };
